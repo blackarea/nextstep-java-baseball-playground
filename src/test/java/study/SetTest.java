bcdb482cc4a39740bce56a4526c0b39bdb28c5fc
blackarea <supersute142@gmail.com>
2022-05-14 19:56:14 +0900

    0. 학습테스트 실습

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void setSizeTest(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void setContainTest(int number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    //어떻게 활용해야 될지 몰라서 구글링 도움 받음
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void setContainDetailTest(int input, Boolean expected){
+        assertThat(numbers.contains(input)).isEqualTo(expected);
+    }
+}
