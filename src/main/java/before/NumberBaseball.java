dc80781c6a8cf0730dd6d8b266015b90dcbb5fde
blackarea <supersute142@gmail.com>
2022-05-24 17:34:10 +0900

    실행 함수 추가
    - 게임 진행 함수 틀 제작

---
+++ src/main/java/before/NumberBaseball.java
@@ -0,0 +1,13 @@
+package before;
+
+public class NumberBaseball {
+
+    public void execute(){
+        System.out.println("숫자야구게임에 오신것을 환영합니다.");
+        gameStart();
+    }
+
+    public void gameStart(){
+
+    }
+}
