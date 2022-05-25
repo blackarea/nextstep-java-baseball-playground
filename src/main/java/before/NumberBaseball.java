cd3d1eec59ef8a720a6b006b8842b0b31c2545de
blackarea <supersute142@gmail.com>
2022-05-25 13:30:51 +0900

    judgeReulst : 사용자가 입력한 숫자가 어떤지 판별하는 메소드
    - judgeStrike
    - judgeBall
    - indent(인덴트, 들여쓰기) depth를 2까지 구현했다.
    - 물론 정해진 규칙대로 1까지로 구현자체는 가능하지만, 가독성이 떨어지고 직관적이지 않다고 생각해서 규칙을 어겼다.
    - 이 부분은 피드백을 본 이후에 고칠 예정이다.

--- src/main/java/before/NumberBaseball.java
+++ src/main/java/before/NumberBaseball.java
@@ -9,28 +9,72 @@ public class NumberBaseball {
 
     }
 
-    public void execute(){
+    public void execute() {
         System.out.println("숫자야구게임에 오신것을 환영합니다.");
         gameStart();
     }
 
-    public void gameStart(){
+    public void gameStart() {
         int[] randomNumberArr = makeRandomNumberArray();
         boolean threeStrike = false;
         int[] inputNumberArr;
-        while (!threeStrike){
+        while (!threeStrike) {
             printMessage();
             inputNumberArr = intToArray(scanNumber());
-            threeStrike = isGameOver(judgeResult(inputNumberArr, randomNumberArr));
+            threeStrike = isGameOver(judgeResult(randomNumberArr, inputNumberArr));
         }
     }
 
-    private String judgeResult(int[] inputNumberArr, int[] randomArr) {
+    public String judgeResult(int[] randomArr, int[] inputNumberArr) {
+        int strikeCount = judgeStrike(randomArr, inputNumberArr);
+        int ballCount = judgeBall(randomArr, inputNumberArr);
 
-        return null;
+        if(ballCount != 0 && strikeCount != 0){
+            return ballCount + "B " + strikeCount + "S";
+        }
+        if(ballCount != 0){
+            return ballCount + "B";
+        }
+        if(strikeCount != 0){
+            return strikeCount + "S";
+        }
+        return "4B";
     }
 
-    public int[] intToArray(int threeNumber){
+    public int judgeStrike(int[] randomArr, int[] inputNumberArr) {
+        int count = 0;
+        for (int i = 0; i < baseballCount; i++) {
+            if(isSameNumber(randomArr[i], inputNumberArr[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int judgeBall(int[] randomArr, int[] inputNumberArr) {
+        int count = 0;
+        for (int i = 0; i < baseballCount; i++) {
+            if(containNumberNotSameIndex(randomArr[i], inputNumberArr, i)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isSameNumber(int number1, int number2) {
+        return number1 == number2;
+    }
+
+    private boolean containNumberNotSameIndex(int number, int[] array, int index) {
+        for (int i = 0; i < baseballCount; i++) {
+            if(isSameNumber(number, array[i]) && i != index){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[] intToArray(int threeNumber) {
         int[] array = new int[baseballCount];
         for (int i = 0; i < baseballCount; i++) {
             array[i] = (int) (threeNumber / Math.pow(10, baseballCount - 1 - i)) % 10;
@@ -45,12 +89,12 @@ public class NumberBaseball {
         int inputNumber;
         try {
             inputNumber = Integer.parseInt(inputString);
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("숫자가 아닌 다른 값이 입력 되었습니다. 다시 입력해주세요");
             return scanNumber();
         }
 
-        if(inputString.length() != 3){
+        if (inputString.length() != 3) {
             System.out.println("본 게임은 3자리 숫자의 숫자 야구 게임입니다. 3자리만 입력해주세요");
             return scanNumber();
         }
@@ -61,7 +105,7 @@ public class NumberBaseball {
         System.out.println("숫자를 입력해주세요 : ");
     }
 
-    private void outMessage(){
+    private void outMessage() {
 
     }
 
@@ -71,7 +115,7 @@ public class NumberBaseball {
         return false;
     }
 
-    public int[] makeRandomNumberArray(){
+    public int[] makeRandomNumberArray() {
         int[] randomNumber = new int[baseballCount];
         for (int i = 0; i < baseballCount; i++) {
             randomNumber[i] = noDuplicateNumber(randomNumber);
@@ -82,7 +126,7 @@ public class NumberBaseball {
     private int noDuplicateNumber(int[] randomNumber) {
         int n = (int) (Math.random() * 9) + 1;
 
-        while (randomNumber[0] == n || randomNumber[1] == n){
+        while (randomNumber[0] == n || randomNumber[1] == n) {
             n = (int) (Math.random() * 9);
         }
         return n;
--- src/test/java/before/NumberBaseballTest.java
+++ src/test/java/before/NumberBaseballTest.java
@@ -27,14 +27,43 @@ class NumberBaseballTest {
     }
 
     @Test
-    void scanNumber(){
+    void scanNumber() {
         inputHandling("123");
         assertThat(nb.scanNumber()).isEqualTo(123);
     }
 
     @Test
-    void intToArray(){
+    void intToArray() {
         int[] arr = nb.intToArray(123);
         assertThat(arr).containsExactly(1, 2, 3);
     }
+
+    @Test
+    void judgeStrike() {
+        int[] randomArr = {1, 2, 3};
+        int[] inputArr = {1, 2, 3};
+        assertThat(nb.judgeStrike(randomArr, inputArr)).isEqualTo(3);
+        inputArr[0] = 5;
+        assertThat(nb.judgeStrike(randomArr, inputArr)).isEqualTo(2);
+    }
+
+    @Test
+    void judgeBall() {
+        int[] randomArr = {1, 2, 3};
+        int[] inputArr = {3, 1, 2};
+        assertThat(nb.judgeBall(randomArr, inputArr)).isEqualTo(3);
+        int[] inputArr2 = {1, 2, 3};
+        assertThat(nb.judgeBall(randomArr, inputArr2)).isEqualTo(0);
+    }
+
+    @Test
+    void judgeResult() {
+        int[] randomArr = {1, 2, 3};
+
+        assertThat(nb.judgeResult(randomArr, new int[]{1, 2, 3})).isEqualTo("3S");
+        assertThat(nb.judgeResult(randomArr, new int[]{3, 1, 2})).isEqualTo("3B");
+        assertThat(nb.judgeResult(randomArr, new int[]{5, 4, 6})).isEqualTo("4B");
+        assertThat(nb.judgeResult(randomArr, new int[]{1, 3, 6})).isEqualTo("1B 1S");
+    }
+
 }
